f0e23fcf1794153f32ba9551716d233895ce6f65
Tonytor <teme4842@gmail.com>
2023-03-22 17:38:01 +0300

    play with drum

---
+++ index.js
@@ -0,0 +1,66 @@
+const numberOfDrumButton = document.querySelectorAll(".drum")
+
+for (let i = 0; i < numberOfDrumButton.length; i++) {
+    
+    numberOfDrumButton[i].addEventListener("click",drumHundler)
+    
+}
+function drumHundler(){
+   const ButtoninnerHTML = this.innerHTML
+   makeSound(ButtoninnerHTML)
+   animateSound(ButtoninnerHTML)
+}
+
+
+document.addEventListener('keypress',pressHandler)
+
+function pressHandler(event){
+    makeSound(event.key)
+    animateSound(event.key)
+
+}
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio('./sounds/tom-1.mp3')
+            tom1.play()
+            break;
+        case "a":
+            var tom2 = new Audio('./sounds/tom-2.mp3')
+            tom2.play()
+            break;  
+        case "s":
+            var tom3 = new Audio('./sounds/tom-3.mp3')
+            tom3.play()
+            break; 
+        case "d":
+             var tom4 = new Audio('./sounds/tom-4.mp3')
+             tom4.play()
+             break;
+        case "j":
+            var snare = new Audio('./sounds/snare.mp3')
+            snare.play()
+            break;  
+        case "k":
+            var crash = new Audio('./sounds/crash.mp3')
+            crash.play()
+            break;
+        case "l":
+            var kick = new Audio('./sounds/kick-bass.mp3')
+            kick.play()
+            break;                            
+        default:
+            console.log(ButtoninnerKey)
+            break;
+       }
+}
+
+function animateSound(curruntKey) {
+    const activeButton = document.querySelector("." + curruntKey)
+    activeButton.classList.add("pressed")
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    },100)
+    
+}
